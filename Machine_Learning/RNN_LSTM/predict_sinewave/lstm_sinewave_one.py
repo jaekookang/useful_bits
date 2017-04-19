5b08b299b50a6aacdd7d48c5d58aff1f807ebeea
Jaekoo Kang <jaekoo.jk@gmail.com>
2017-04-19 14:13:52 -0400

    update

--- Machine_Learning/RNN_LSTM/predict_sinewave/lstm_sinewave_one.py
+++ Machine_Learning/RNN_LSTM/predict_sinewave/lstm_sinewave_one.py
@@ -1,10 +1,10 @@
-# Simple RNN-LSTM regression
+# Simple LSTM regression
 # 2017-03-16 jkang
 # Python3.5
 # Tensorflow1.0.1
 #
 # input: one sinewave
-# output: one sinewave
+# output: one sinewave (shifted input)
 
 import tensorflow as tf
 import numpy as np
@@ -12,31 +12,31 @@ import matplotlib.pyplot as plt
 
 # Input, Ouput dataset
 duration = 10  # sec
-srate = 10  # Hz
+srate = 100  # Hz
 freq = .5  # Hz
 amplitude = np.random.random(1) * 10
 t = np.linspace(0, duration, duration * srate + 1)
 sin = np.sin(2 * np.pi * freq * t) * amplitude
-shift = int(srate/freq*1/4) # samples shifted for orthogonally align the output with the input
+shift = int(srate/freq*1/4) # 1/4 phase shift to make input & output orthogonal
 sin_in = sin[:-shift]
 sin_out = sin[shift:]  # shifting
 
 # Hyper-Parameters
 learning_rate = 0.01
-max_iter = 100
+max_iter = 20
 
 # Network Parameters
 n_input_dim = 1
 n_input_len = len(sin_in)
 n_output_len = len(sin_out)
-n_hidden = 50
+n_hidden = 100
 n_output_dim = 1
 
 # TensorFlow graph
 # (batch_size) x (time_step) x (input_dimension)
-x = tf.placeholder(tf.float32, [1, n_input_len, n_input_dim])
+x_data = tf.placeholder(tf.float32, [1, n_input_len, n_input_dim])
 # (batch_size) x (time_step) x (output_dimension)
-y = tf.placeholder(tf.float32, [1, n_output_len, n_output_dim])
+y_data = tf.placeholder(tf.float32, [1, n_output_len, n_output_dim])
 
 # Parameters
 weights = {
@@ -46,38 +46,48 @@ biases = {
     'out': tf.Variable(tf.random_normal([n_output_dim]))
 }
 
+
 def RNN(x, weights, biases):
-    lstm = tf.contrib.rnn.BasicLSTMCell(n_hidden, forget_bias=1.0)
-    outputs, states = tf.nn.dynamic_rnn(lstm, x, dtype=tf.float32)
+    cell = tf.contrib.rnn.BasicLSTMCell(n_hidden, forget_bias=1.0) # Make LSTMCell
+    outputs, states = tf.nn.dynamic_rnn(cell, x, time_major=False, dtype=tf.float32)
+    '''
+    **Notes on tf.nn.dynamic_rnn**
+
+    - 'x' can have shape (batch)x(time)x(input_dim), if time_major=Flase or 
+                         (time)x(batch)x(input_dim), if time_major=True
+    - 'outputs' can have the same shape as 'x'
+                         (batch)x(time)x(input_dim), if time_major=Flase or 
+                         (time)x(batch)x(input_dim), if time_major=True
+    - 'states' is the final state, determined by batch and hidden_dim
+    '''
+    
+    # outputs[-1] is outputs for the last example in the mini-batch
     return tf.matmul(outputs[-1], weights['out']) + biases['out']
 
-pred = RNN(x, weights, biases)
-cost = tf.reduce_mean(tf.squared_difference(pred, y))
+pred = RNN(x_data, weights, biases)
+cost = tf.reduce_mean(tf.squared_difference(pred, y_data))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
 
 init = tf.global_variables_initializer()
 with tf.Session() as sess:
     sess.run(init)
     step = 1
     while step <= max_iter:
-        train_x = sin_in.reshape((1, n_input_len, n_input_dim))
-        train_y = sin_out.reshape((1, n_output_len, n_output_dim))
-        c, _ = sess.run([cost, optimizer], feed_dict={x: train_x, y: train_y})
+        x_train = sin_in.reshape((1, n_input_len, n_input_dim))
+        y_train = sin_out.reshape((1, n_output_len, n_output_dim))
+        c, _ = sess.run([cost, optimizer], feed_dict={x_data: x_train, y_data: y_train})
         print('Epoch =', str(step), '/', str(max_iter),
               'Cost = ', '{:.5f}'.format(c))
         step += 1
 
     # Test
-    pred_out = sess.run(pred, feed_dict={x: train_x})
+    pred_out = sess.run(pred, feed_dict={x_data: x_train})
+
 
 # Plot
 f, axes = plt.subplots(2, sharey=True)
-axes[0].plot(t[:-shift], sin_out, 'o')
-axes[1].plot(t[:-shift], pred_out, 'o')
+axes[0].plot(t[:-shift], sin_out)
+axes[1].plot(t[:-shift], pred_out)
 plt.show()
 
-
-# ### Further thoughts
-# 
-# * For future, the proper number of shifting should be decided. Here, I tried to shift samples to make the output to be orthogonal to the input in correlation. 
-# * Windowing might be needed to learn more local shapes
--- Machine_Learning/RNN_LSTM/predict_sinewave/rnn_sinewave_one.py
+++ Machine_Learning/RNN_LSTM/predict_sinewave/rnn_sinewave_one.py
@@ -1,10 +1,10 @@
-# Simple RNN-LSTM regression
+# Simple RNN regression
 # 2017-03-16 jkang
 # Python3.5
 # Tensorflow1.0.1
 #
 # input: one sinewave
-# output: one sinewave (one sample shifted)
+# output: one sinewave (shifted input)
 
 import tensorflow as tf
 import numpy as np
@@ -18,7 +18,7 @@ freq = .5  # Hz
 amplitude = np.random.random(1) * 10
 t = np.linspace(0, duration, duration * srate + 1)
 sin = np.sin(2 * np.pi * freq * t) * amplitude
-shift = 10
+shift = int(srate/freq/4) # 1/4 phase shift to make input & output orthogonal
 sin_in = sin[:-shift]
 sin_out = sin[shift:]  # shifting
 
@@ -30,14 +30,14 @@ max_iter = 20
 n_input_dim = 1
 n_input_len = len(sin_in)
 n_output_len = len(sin_out)
-n_hidden = 50
+n_hidden = 100
 n_output_dim = 1
 
 # TensorFlow graph
 # (batch_size) x (time_step) x (input_dimension)
-x = tf.placeholder(tf.float32, [1, n_input_len, n_input_dim])
+x_data = tf.placeholder(tf.float32, [1, n_input_len, n_input_dim])
 # (batch_size) x (time_step) x (output_dimension)
-y = tf.placeholder(tf.float32, [1, n_output_len, n_output_dim])
+y_data = tf.placeholder(tf.float32, [1, n_output_len, n_output_dim])
 
 # Parameters
 weights = {
@@ -49,12 +49,24 @@ biases = {
 
 
 def RNN(x, weights, biases):
-    lstm = tf.contrib.rnn.BasicLSTMCell(n_hidden, forget_bias=1.0)
-    outputs, states = tf.nn.dynamic_rnn(lstm, x, dtype=tf.float32)
+    cell = tf.contrib.rnn.BasicRNNCell(n_hidden) # Make RNNCell
+    outputs, states = tf.nn.dynamic_rnn(cell, x, time_major=False, dtype=tf.float32)
+    '''
+    **Notes on tf.nn.dynamic_rnn**
+
+    - 'x' can have shape (batch)x(time)x(input_dim), if time_major=Flase or 
+                         (time)x(batch)x(input_dim), if time_major=True
+    - 'outputs' can have the same shape as 'x'
+                         (batch)x(time)x(input_dim), if time_major=Flase or 
+                         (time)x(batch)x(input_dim), if time_major=True
+    - 'states' is the final state, determined by batch and hidden_dim
+    '''
+    
+    # outputs[-1] is outputs for the last example in the mini-batch
     return tf.matmul(outputs[-1], weights['out']) + biases['out']
 
-pred, vals = RNN(x, weights, biases)
-cost = tf.reduce_mean(tf.squared_difference(pred, y))
+pred = RNN(x_data, weights, biases)
+cost = tf.reduce_mean(tf.squared_difference(pred, y_data))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
 
@@ -63,19 +75,18 @@ with tf.Session() as sess:
     sess.run(init)
     step = 1
     while step <= max_iter:
-        train_x = sin_in.reshape((1, n_input_len, n_input_dim))
-        train_y = sin_out.reshape((1, n_output_len, n_output_dim))
-        c, _ = sess.run([cost, optimizer], feed_dict={x: train_x, y: train_y})
+        x_train = sin_in.reshape((1, n_input_len, n_input_dim))
+        y_train = sin_out.reshape((1, n_output_len, n_output_dim))
+        c, _ = sess.run([cost, optimizer], feed_dict={x_data: x_train, y_data: y_train})
         print('Epoch =', str(step), '/', str(max_iter),
               'Cost = ', '{:.5f}'.format(c))
         step += 1
 
     # Test
-    pred_out = sess.run(pred, feed_dict={x: train_x})
+    pred_out = sess.run(pred, feed_dict={x_data: x_train})
 
 # Plot
 f, axes = plt.subplots(2, sharey=True)
 axes[0].plot(t[:-shift], sin_out)
 axes[1].plot(t[:-shift], pred_out)
 plt.show()
-
